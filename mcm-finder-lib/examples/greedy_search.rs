6e9a34d4fde5ac5083388180ebd251d7580f4077
pluumenbrownie <git.wessel@beumertjes.nl>
2026-05-09 04:09:51 +0200

    greedy lookahead now works. probably

--- mcm-finder-lib/examples/greedy_search.rs
+++ mcm-finder-lib/examples/greedy_search.rs
@@ -5,14 +5,11 @@ use miette::Result;
 use mcm_finder_lib::solvers::{GreedySearcher, Solver};
 
 fn main() -> Result<()> {
-    // let dataset = Dataset::read_from_file(Path::new(
-    //     "mcm-finder-lib/tests/data/SCOTUS_n9_N895_Data.dat",
-    // ))
-    // .unwrap();
+    // let filepath = Path::new("mcm-finder-lib/tests/data/SCOTUS_n9_N895_Data.dat");
     let filepath = Path::new("mcm-finder-lib/tests/data/MNIST11.sorted");
     // let filepath = Path::new("mcm-finder-lib/tests/data/Big5PT.sorted");
 
-    let solver = GreedySearcher::from_file(filepath)?;
+    let solver = GreedySearcher::from_file(filepath)?.lookahead(0);
     // let solver = GreedySearcher::from_file(filepath)?.continue_after_minimum();
     let result = solver.solve();
     println!("{}", result);
--- mcm-finder-lib/src/solvers/greedy.rs
+++ mcm-finder-lib/src/solvers/greedy.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::HashMap,
-    marker::Sized,
-    num::{NonZero, NonZeroUsize},
-    path::Path,
-};
+use std::{collections::HashMap, marker::Sized, num::NonZero, path::Path};
 
 use fixedbitset::FixedBitSet;
 use kdam::{Bar, BarExt, tqdm};
@@ -43,12 +38,20 @@ impl LogeMCM {
             deep_log_e: log_e,
         }
     }
+
+    pub fn set_deep_log_e(self, deep_log_e: f64) -> Self {
+        Self {
+            mcm: self.mcm,
+            log_e: self.log_e,
+            deep_log_e,
+        }
+    }
 }
 
 #[derive(Clone)]
 pub struct GreedySearcher {
     dataset: VecDataset,
-    lookahead: Option<NonZeroUsize>,
+    lookahead_depth: usize,
     continue_after_minimum: bool,
 }
 
@@ -62,14 +65,16 @@ impl GreedySearcher {
 
     /// Sets the amount of steps the algorithm is allowed to look ahead to choose the
     /// next best candidate.
+    ///
+    /// Setting this value will explode the execution time of this algorithm, so use
+    /// with caution.
     pub fn lookahead(mut self, depth: usize) -> Self {
-        self.lookahead =
-            Some(NonZeroUsize::try_from(depth).expect("Lookahead depth should not be zero."));
+        self.lookahead_depth = depth;
         self
     }
 
     fn update_global_best(&self, best_mcm: &mut LogeMCM, candidate: &LogeMCM) -> bool {
-        if candidate.log_e.total_cmp(&best_mcm.log_e).is_ge() {
+        if candidate.log_e.total_cmp(&best_mcm.log_e).is_gt() {
             *best_mcm = candidate.clone();
             return true;
         };
@@ -77,45 +82,80 @@ impl GreedySearcher {
     }
 
     #[must_use]
-    fn find_lowest_merge(
+    fn find_best_merge(
         &self,
         log_e_cache: &mut Option<HashMap<FixedBitSet, f64>>,
         progress: &mut Bar,
-        parts_left: usize,
-        old_best: &LogeMCM,
-    ) -> LogeMCM {
-        let mut gen_best: Option<LogeMCM> = None;
-        for basis in 1..=parts_left {
-            for into in 0..basis {
-                let candidate =
-                    LogeMCM::calculate(old_best.mcm.merge(basis, into), &self.dataset, log_e_cache);
+        iccs_left: usize,
+        original: &LogeMCM,
+        depth: usize,
+    ) -> Option<LogeMCM> {
+        if depth > self.lookahead_depth || iccs_left == 0 {
+            None
+        } else {
+            let mut gen_best: Option<LogeMCM> = None;
+            for basis in 1..=iccs_left {
+                for into in 0..basis {
+                    let mut candidate = LogeMCM::calculate(
+                        original.mcm.merge(basis, into),
+                        &self.dataset,
+                        log_e_cache,
+                    );
 
-                match &gen_best {
-                    Some(best) => {
-                        if candidate.deep_log_e.total_cmp(&best.deep_log_e).is_ge() {
-                            gen_best = Some(candidate.clone());
-                        }
+                    // lookahead
+                    if let Some(deep_best) = self.find_best_merge(
+                        log_e_cache,
+                        progress,
+                        iccs_left - 1,
+                        &candidate,
+                        depth + 1,
+                    ) {
+                        let deep_log_e = deep_best.deep_log_e;
+                        candidate = update_deep_log_e_only(candidate, deep_log_e);
                     }
-                    None => gen_best = Some(candidate.clone()),
-                }
 
-                let _ = progress.update(1);
+                    gen_best = update_if_deep_log_e_better(gen_best, &candidate);
+
+                    let _ = progress.update(1);
+                }
             }
+            gen_best
         }
-        gen_best.unwrap()
     }
 
     fn count_calculations(&self) -> usize {
         let mut length = 0usize;
+        let depth = self.lookahead_depth;
+
         for parts_left in (0usize..self.dataset.variables()).rev() {
-            for basis in 1..=parts_left {
-                for _ in 0..basis {
-                    length += 1;
-                }
-            }
+            count_calculations_recursive(&mut length, parts_left, depth);
         }
         length
     }
+}
+fn count_calculations_recursive(length: &mut usize, parts_left: usize, depth: usize) {
+    for basis in 1..=parts_left {
+        for _ in 0..basis {
+            *length += 1;
+            if depth > 0 {
+                count_calculations_recursive(length, parts_left - 1, depth - 1);
+            }
+        }
+    }
+}
+
+fn update_deep_log_e_only(candidate: LogeMCM, deep_log_e: f64) -> LogeMCM {
+    if deep_log_e.total_cmp(&candidate.deep_log_e).is_gt() {
+        candidate.set_deep_log_e(deep_log_e)
+    } else {
+        candidate
+    }
+}
+
+fn update_if_deep_log_e_better(gen_best: Option<LogeMCM>, candidate: &LogeMCM) -> Option<LogeMCM> {
+    gen_best
+        .filter(|gen_best| gen_best.deep_log_e.total_cmp(&candidate.deep_log_e).is_ge())
+        .or(Some(candidate.clone()))
 }
 
 impl Solver for GreedySearcher {
@@ -125,7 +165,7 @@ impl Solver for GreedySearcher {
     {
         Ok(GreedySearcher {
             dataset: VecDataset::read_from_file(filepath)?,
-            lookahead: None,
+            lookahead_depth: 0,
             continue_after_minimum: false,
         })
     }
@@ -144,11 +184,12 @@ impl Solver for GreedySearcher {
 
         // we merge one partition each round
         let mut progress = tqdm!(total = length);
-        for parts_left in (0usize..self.dataset.variables()).rev() {
-            let old_best = gen_best.clone();
-            progress.set_description(format!("{parts_left} steps left"));
-            gen_best =
-                self.find_lowest_merge(&mut log_e_cache, &mut progress, parts_left, &old_best);
+        for iccs_left in (0usize..self.dataset.variables()).rev() {
+            let original = gen_best.clone();
+            progress.set_description(format!("{iccs_left} ICCs"));
+            gen_best = self
+                .find_best_merge(&mut log_e_cache, &mut progress, iccs_left, &original, 0)
+                .unwrap();
             let new_best = self.update_global_best(&mut best_mcm, &gen_best);
             if !new_best & !self.continue_after_minimum {
                 break;
