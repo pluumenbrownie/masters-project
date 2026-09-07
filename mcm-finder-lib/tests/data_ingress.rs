6b668c82fbe3801e530bc6a4a94ba6b82b22c4cf
pluumenbrownie <git.wessel@beumertjes.nl>
2026-09-07 23:37:03 +0200

    added empty data file test case

--- mcm-finder-lib/tests/data_ingress.rs
+++ mcm-finder-lib/tests/data_ingress.rs
@@ -7,10 +7,12 @@ use mcm_finder_lib::dataset::{Dataset, simple::VecDataset};
 #[test]
 fn read_data() {
     let dataset = VecDataset::read_from_file(Path::new("./tests/data/SCOTUS_n9_N895_Data.dat"));
-    println!("{:?}", dataset);
+    // println!("{:?}", dataset);
     assert!(dataset.is_ok());
-
     let unwrapped_dataset = dataset.unwrap();
+    for (line, nr) in unwrapped_dataset.iter() {
+        println!("{line} - {nr}");
+    }
 
     assert_eq!(
         unwrapped_dataset.get(&FixedBitSet::with_capacity_and_blocks(9, vec![0b111111111])),
@@ -20,11 +22,13 @@ fn read_data() {
         unwrapped_dataset.get(&FixedBitSet::with_capacity_and_blocks(9, vec![0])),
         Some(174)
     );
+    assert_eq!(unwrapped_dataset.variable_states(), 2);
+    assert_eq!(unwrapped_dataset.variable_width(), 1);
 }
 
 #[test]
-fn read_bad_data() {
-    let dataset = VecDataset::read_from_file(Path::new("./tests/data/SCOTUS_n9_N11_bad_data.dat"));
+fn read_empty() {
+    let dataset = VecDataset::read_from_file(Path::new("./tests/data/empty.dat"));
     println!("{:?}", dataset);
     assert!(dataset.is_err())
 }
